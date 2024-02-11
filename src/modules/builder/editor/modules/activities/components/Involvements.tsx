4aa685b6ee5cdccae9d40bb99a4114206dd5264c
Your Name <you@example.com>
2024-02-11 22:47:37 +0530

    removed involvements

--- src/modules/builder/editor/modules/activities/components/Involvements.tsx
+++ src/modules/builder/editor/modules/activities/components/Involvements.tsx
@@ -3,17 +3,17 @@ import { RichtextEditor } from 'src/helpers/common/components/richtext';
 import { useActivity } from 'src/stores/activity';
 
 const Involvements = () => {
-  const activities = useActivity((state) => state.activities);
-  return (
-    <RichtextEditor
-      label="Involvements"
-      value={activities.involvements}
-      onChange={(htmlOutput) => {
-        useActivity.getState().updateInvolvements(htmlOutput);
-      }}
-      name="involvements"
-    />
-  );
+  // const activities = useActivity((state) => state.activities);
+  // return (
+  //   <RichtextEditor
+  //     label="Involvements"
+  //     value={activities.involvements}
+  //     onChange={(htmlOutput) => {
+  //       useActivity.getState().updateInvolvements(htmlOutput);
+  //     }}
+  //     name="involvements"
+  //   />
+  // );
 };
 
 export default Involvements;
